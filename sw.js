294c182407ff545b231396efd7d684a89efb489c
pawanmoondra-arch <pawanmoondra@gmail.com>
2026-08-17 22:25:39 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="ranglekhaa-v22-direct-lock";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim().then(()=>caches.keys()).then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
