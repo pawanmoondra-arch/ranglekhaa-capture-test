1409fa71a7fd41d3109c9d4b52ba23502908ba2d
pawanmoondra-arch <pawanmoondra@gmail.com>
2026-08-18 09:42:01 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE="ranglekhaa-v3-3-fast";
+const CORE=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin && (u.pathname.endsWith("/index.html")||u.pathname.endsWith("/manifest.json"))){
+    e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
